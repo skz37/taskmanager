53069608c6535b15c9e6d4a2e0a5ddb7650e8e7d
SKZ <abdessamadelgahss1@gmail.com>
2026-09-09 20:39:01 +0100

    adding entitys Project,Workspace,Notifications,WorkspaceMember

---
+++ src/TaskManager.Domain/Entities/Notification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.Common;
+
+namespace TaskManager.Domain.Entities
+{
+    internal public class Notification : BaseEntity
+    {
+        public Guid UserId { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Message { get; private set; }
+
+        public NotificationType Type { get; private set; }
+
+        public bool IsRead { get; private set; }
+
+        public Guid? TaskId { get; private set; }
+
+        public Guid? ProjectId { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+    }
+}
---
+++ src/TaskManager.Domain/Entities/WorkspaceMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.Enums;
+
+namespace TaskManager.Domain.Entities
+{
+    internal public class WorkspaceMember
+    {
+        public Guid WorkspaceId { get; private set; }
+
+        public Guid UserId { get; private set; }
+
+        public WorkspaceRole Role { get; private set; }
+    }
+}
