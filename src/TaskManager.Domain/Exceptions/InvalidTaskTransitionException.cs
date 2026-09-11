141c9bd0276f4cef87ed5ee19a294e7203d678d2
SKZ <abdessamadelgahss1@gmail.com>
2026-09-11 22:40:49 +0100

    Creating TaskItem entity - with no test

---
+++ src/TaskManager.Domain/Exceptions/InvalidTaskTransitionException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Domain.Enums;
+
+namespace TaskManager.Domain.Exceptions
+{
+   
+        public class InvalidTaskTransitionException : DomainExceptions
+        {
+            public InvalidTaskTransitionException(
+                TaskStatu currentStatus,
+                TaskStatu newStatus)
+                : base(
+                    $"Transition impossible : {currentStatus} -> {newStatus}.")
+            {
+            }
+        }
+    }
